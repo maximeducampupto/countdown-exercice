482027a881cfa346f7fccee99926ab6f76291a13
Max <maximeducampupto@gmail.com>
2018-11-19 22:17:07 +0100

    - JS refait en version procédurale, à refaire plus tard en objet - Commencé le travail sur le user input

--- js/script.js
+++ js/script.js
@@ -1,38 +1,80 @@
-let timer = {
+let minutesContainer = document.getElementById('timer-minutes'),
+    secondsContainer = document.getElementById('timer-seconds'),
+    startButton = document.getElementById('start-button'),
+    stopButton = document.getElementById('stop-button'),
+    resetButton = document.getElementById('reset-button'),
+    setTimerButton = document.getElementById('set-timer-button'),
+    counter = 0,
+    from = 300,
+    timer,
+    running = false;
 
-    display: document.getElementById('time'),
-    startButton: document.getElementById('start-button'),
-    minutes: '10',
-    seconds: '30',
-    total: null,
+minutesContainer.innerHTML = '05';
+secondsContainer.innerHTML = '00';
 
-    init: function()
+function getMinutes()
+{
+    let minutes =  Math.floor((from - counter) / 60);
+    if (minutes >= 0 && minutes < 10)
     {
-        this.display.innerHTML = this.minutes + ":" + this.seconds;
-
-        timer.total = parseInt((timer.minutes * 60) + parseInt(timer.seconds));
-        this.startButton.addEventListener('click', this.run);
-    },
-
-    run: function()
-    {
-
-        setTimeout(function() {
-            timer.total--;
-            timer.update();
-            timer.run();
-        }, 1000);
-
-    },
-
-    update: function()
-    {
-        let minutes = Math.floor(timer.total % 3600 / 60),
-            seconds =  Math.floor(timer.total % 3600 % 60);
-
-        timer.display.innerHTML = minutes+":"+seconds;
+        return "0" + minutes;
     }
-};
+
+    return minutes;
+}
+
+function getSeconds()
+{
+    let seconds = Math.floor((from - counter) % 60);
+    if (seconds >= 0 && seconds < 10)
+    {
+        return "0" + seconds;
+    }
+
+    return seconds;
+}
 
 
-timer.init();
+function loop()
+{
+   if (running && counter < from)
+   {
+       console.log(counter);
+
+       timer = setTimeout(function()
+       {
+           counter++;
+           minutesContainer.innerHTML = getMinutes();
+           secondsContainer.innerHTML = getSeconds();
+           loop();
+       }, 1000);
+   }
+}
+
+startButton.addEventListener('click', function() {
+    if (!running) {
+        running = true;
+        loop();
+    }
+});
+stopButton.addEventListener('click', function() {
+    running = false;
+    clearTimeout(timer);
+});
+
+resetButton.addEventListener('click', function() {
+    running = false;
+   clearTimeout(timer);
+   counter = 0;
+   from = 300;
+   minutesContainer.innerHTML = "05";
+   secondsContainer.innerHTML = "00";
+});
+
+setTimerButton.addEventListener('click', function() {
+    document.getElementById('timer').style.display = "none";
+    document.getElementById('buttons').style.display = "none";
+    document.getElementById('container').style.justifyContent = "center";
+   document.getElementById('input-field').style.display = "flex";
+
+});
